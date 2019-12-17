a332f4279edc10da783780f138968df7243305dc
Vasily Yurchenko <vasily.v.yurchenko@yandex.ru>
2019-12-17 19:02:51 +0300

    [A] базовый код: добавлены функции-шаблоны.

--- src/main.c
+++ src/main.c
@@ -13,6 +13,67 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <getopt.h>
+#include <stdio.h>
+#include <signal.h>
+
+/**
+ *
+ */
+static void display_usage(void)
+{
+	printf("Вывод информации по использонию программы.\n");
+	/* TODO: Реализовать. */
+}
+
+uint32_t exit_flag = 0;
+static void handleSignal(int32_t sigNum) {
+
+	switch (sigNum) {
+		case SIGINT:
+		printf("Someone wants to stop application\n");
+		exit_flag = 1;
+		break;
+	default:
+		printf("Unregistered signal received: %d\n", sigNum);
+		exit(-1);
+		break;
+	}
+}
+
+/**
+ *
+ */
+static int sig_handler_init(void)
+{
+	/* TODO: Реализовать. */
+	struct sigaction act;
+	act.sa_handler = handleSignal;
+	return sigaction(SIGINT, &act, NULL);
+}
+
+/**
+ *
+ */
+static int daemon_start(void)
+{
+	/* TODO: Реализовать. */
+}
+
+/**
+ *
+ */
+static int client_start(void)
+{
+	/* TODO: Реализовать. */
+}
+
+/**
+ *
+ */
+static int service_start(void)
+{
+	/* TODO: Реализовать. */
+}
 
 int main(uint32_t argc, char *argv[])
 {
@@ -26,7 +87,7 @@ int main(uint32_t argc, char *argv[])
 	 * 		-с запускается процесс, который осуществляет
 	 * 		прием идентификационных данных по UDP;
 	 *
-	 * 		-k завершить запущенный процесс-демон.
+	 * 		-s завершить запущенный процесс-демон.
 	 *
 	 * 2. Если активирован режим демона, то:
 	 *
@@ -68,6 +129,46 @@ int main(uint32_t argc, char *argv[])
 	 *
 	 *		отправить сообщение о завершении работы процессу с идентификатором PID.
 	 */
-	fork();
+
+	/** Перечисление режимов работы программы. */
+	enum mdoe {
+		unknown = 0,	/**< Начальный неопределенный режим работы программы. */
+		daemon,			/**< Режим демона, в котором происходит циклическая передача. */
+		client,			/**< Режим клиента, в котором происходит прием.*/
+		service			/**< Сервисный режим, в котором происходит остановка демона.*/
+	};
+
+	enum mode cur_mode = unknown;
+
+	const char options[] = "dcs";
+	int opt = getopt( argc, argv, options );
+	while (opt != -1) {
+		switch (opt) {
+			case 'd':
+				cur_mode = daemon;
+				printf("Активирован режим демона.\n");
+				break;
+
+			case 'c':
+				cur_mode = client;
+				printf("Активирован режим клиента.\n");
+				break;
+
+			case 'k':
+				cur_mode = service;
+				printf("Активирован сервисный режим.\n");
+				break;
+
+			case 'h':
+			case '?':
+			default:
+
+				display_usage();
+				break;
+		}
+		if (cur_mode != unknown)
+			break;
+	}
+
 	return 0;
 }
