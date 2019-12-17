99f26a33cf73af2f3213af5522aefa97226033c0
Vasily Yurchenko <vasily.v.yurchenko@yandex.ru>
2019-12-17 14:49:14 +0300

    [A] структура: сформирована общая структура, организована сборка.

---
+++ src/main.c
@@ -0,0 +1,17 @@
+/**
+ * @file	src/main.c
+ *
+ * @brief	Точка входа.
+ *
+ * @author	Vasily Yurchenko <vasily.v.yurchenko@yandex.ru>
+ *
+ *
+ *
+ */
+
+#include <stdint.h>
+
+int main(uint32_t argc, char *argv[])
+{
+	return 0;
+}
